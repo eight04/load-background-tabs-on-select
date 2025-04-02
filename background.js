01d5b70ccc8cabbc5eb6da43eacf942c7d27ef13
igorlogius <67047467+igorlogius@users.noreply.github.com>
2025-04-02 13:35:43 +0200

    this seems fine

--- background.js
+++ background.js
@@ -81,136 +81,6 @@ async function onStorageChange() {
   // register listeners
   // -------------------------------
 
-  browser.storage.onChanged.addListener(onStorageChange);
-
-  async function onBeforeRequest(e) {
-    if (manually_disabled) {
-      return;
-    }
-
-    if (wasActive.has(e.tabId)) {
-      return;
-    }
-
-    const reqTab = await browser.tabs.get(e.tabId);
-    if (reqTab.active) {
-      wasActive.add(e.tabId);
-      return;
-    }
-
-    const mre = matchesRegEx(e.url);
-
-    if (
-      (mode && mre) || // blacklist(true) => matches are not allowed to load
-      (!mode && !mre) // whitelist(false) => matches are allowed to load <=> no match => not allowed
-    ) {
-      // Instead of canceling the request, we just rewrite the response
-      // this way we still get the title,
-      // the resources that a tab will allocate should be close to nothing
-      // and it should have a stable/reloadable state
-      console.debug("reload set");
-
-      // NOTE: awaitsReload should always be set before the user is able to initalize a focus switch to the created yet unfocused tab
-      // no documentation seems to explicitly state that this has to be the case so this is an assumption currenlty only supported by tests
-      awaitsReload.set(e.tabId, e.url);
-
-      let filter = await browser.webRequest.filterResponseData(e.requestId);
-
-      filter.ondata = (event) => {
-        let str = decoder.decode(event.data, { stream: true });
-        const doc = parser.parseFromString(str, "text/html");
-        str = `<!doctype html>
-<head>
-  <title>${doc.title}</title>
-</head>
-<body>
-<h1>trying to load now, please wait ... </h1>
-</body>
-</html>`;
-        //console.debug(str);
-        filter.write(encoder.encode(str));
-        filter.close(); // close filter/stream
-      };
-      // dont add to wasActive here
-      return;
-    }
-    // not really, but lets treat it like it has been activated
-    wasActive.add(e.tabId);
-  }
-
-  browser.webRequest.onBeforeRequest.addListener(
-    onBeforeRequest,
-    {
-      urls: ["<all_urls>"],
-      types: ["main_frame"],
-    },
-    ["blocking"],
-  );
-
-  /*
-  async function onHeadersReceived(e) {
-    if (!manually_disabled) {
-      if (!wasActive.has(e.tabId)) {
-        const reqTab = await browser.tabs.get(e.tabId);
-        if (!reqTab.active) {
-          const mre = matchesRegEx(
-            typeof e.originUrl === "undefined" ? e.url : e.originUrl,
-          );
-
-          if (
-            (mode && mre) || // blacklist(true) => matches are not allowed to load
-            (!mode && !mre) // whitelist(false) => matches are allowed to load <=> no match => not allowed
-          ) {
-            return { responseHeaders: [] };
-          }
-        }
-      }
-      wasActive.add(e.tabId);
-    }
-  }
-
-  browser.webRequest.onHeadersReceived.addListener(
-    onHeadersReceived,
-    {
-      urls: ["<all_urls>"],
-      types: [
-        "main_frame",
-        "sub_frame",
-        "stylesheet",
-        "script",
-        "image",
-        "font",
-        "object",
-        "xmlhttprequest",
-        "ping",
-        "csp_report",
-        "media",
-        "websocket",
-        "other",
-      ],
-    },
-    ["blocking", "responseHeaders"],
-  );
-*/
-
-  // ----
-  // a newly created background tab should not be activatable before onBeforeRequest was called
-  // if it does, the awaitsReload is not set and the load can not be triggered
-  browser.tabs.onActivated.addListener(async (activeInfo) => {
-    //console.debug('switched focus');
-    if (!wasActive.has(activeInfo.tabId)) {
-      wasActive.add(activeInfo.tabId);
-      // awaitsReload should always be set before we can switch the focus
-      setTimeout(() => {
-        if (awaitsReload.has(activeInfo.tabId)) {
-          browser.tabs.update(activeInfo.tabId, {
-            url: awaitsReload.get(activeInfo.tabId),
-          });
-        }
-      }, 2000); // 2secs seems fine ...
-    }
-  });
-
   browser.tabs.onRemoved.addListener((tabId) => {
     if (wasActive.has(tabId)) {
       wasActive.delete(tabId);
@@ -222,9 +92,74 @@ async function onStorageChange() {
     setToStorage("manually_disabled", manually_disabled);
   });
 
-  browser.tabs.onCreated.addListener((tab) => {
-    if (tab.active) {
-      wasActive.add(tab.id);
+  browser.storage.onChanged.addListener(onStorageChange);
+
+  browser.webRequest.onBeforeRequest.addListener(
+    async (e) => {
+      if (!manually_disabled) {
+        if (!wasActive.has(e.tabId)) {
+          const reqTab = await browser.tabs.get(e.tabId);
+          if (!reqTab.active) {
+            const mre = matchesRegEx(e.url);
+
+            if (
+              (mode && mre) || // blacklist(true) => matches are not allowed to load
+              (!mode && !mre) // whitelist(false) => matches are allowed to load <=> no match => not allowed
+            ) {
+              // Instead of canceling the request, we just rewrite the response
+              // this way we still get the title,
+              // the resources that a tab will allocate should be close to nothing
+              // and it should have a stable state
+
+              // NOTE: awaitsReload should always be set before the user is able to initalize a focus switch to the yet unfocused tab
+              // no documentation seems to explicitly state that this has to be the case so this is an assumption currenlty only supported by tests
+              awaitsReload.set(e.tabId, e.url);
+
+              let filter = await browser.webRequest.filterResponseData(
+                e.requestId,
+              );
+
+              filter.ondata = (event) => {
+                let str = decoder.decode(event.data, { stream: true });
+                const doc = parser.parseFromString(str, "text/html");
+                const docTitle = doc.title
+                  .split("/[<>\\ ]/") // little bit of sanatizing
+                  .join(" ")
+                  .replaceAll(/\s+/g, " ");
+                // https://validator.w3.org/nu/ => No errors or warnings to show.
+                str = `<!DOCTYPE html><html lang="en"><head><title>${docTitle}</title></head><body><h1>Loading now, please wait...</h1></body></html>`;
+                filter.write(encoder.encode(str));
+                filter.close(); // close filter ... disconnect would allow extra data, which we dont want
+              };
+              // dont add to wasActive here
+              return;
+            }
+          }
+          // not really, but lets treat it like it has been activated
+          wasActive.add(e.tabId);
+        }
+      }
+    },
+    {
+      urls: ["<all_urls>"],
+      types: ["main_frame"],
+    },
+    ["blocking"],
+  ); // << onBeforeRequest
+
+  // ----
+  // a newly created background tab should not be activatable before onBeforeRequest was called
+  // if it does, the awaitsReload is not set and the load can not be triggered
+  browser.tabs.onActivated.addListener(async (activeInfo) => {
+    if (!wasActive.has(activeInfo.tabId)) {
+      wasActive.add(activeInfo.tabId);
+      setTimeout(() => {
+        if (awaitsReload.has(activeInfo.tabId)) {
+          browser.tabs.update(activeInfo.tabId, {
+            url: awaitsReload.get(activeInfo.tabId),
+          });
+        }
+      }, 2000); // 2secs seems fine ...
     }
   });
 })();
